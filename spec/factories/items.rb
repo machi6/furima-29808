41e420a6e00bfb2f7894e21b67c6f619d1911bd1
machi6 <yosukehonma01@gmail.com>
2020-09-09 23:41:16 +0900

    itemモデルに対する単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+
+    user_id {1}
+    name {'革ジャン'}
+    description {'一度しか着ていない革ジャンです。'}
+    category_id {2}
+    condition_id {2}
+    delivery_fee_payer_id {2}
+    shipping_origin_prefecture_id {2}
+    lead_time_id {2}
+    price {300}
   end
 end
