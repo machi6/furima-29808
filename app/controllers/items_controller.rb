a730219c88356c923c54441e0984bee6e6622fd8
machi6 <yosukehonma01@gmail.com>
2020-09-10 14:41:05 +0900

    新規投稿を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,27 @@
 class ItemsController < ApplicationController
   def index
+    #binding.pry
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(
-      :user_id, 
+    params.permit(
+    #params.require(:item).permit(
       :name, 
       :price, 
       :description, 
@@ -13,8 +29,8 @@ class ItemsController < ApplicationController
       :condition_id, 
       :delivery_fee_payer_id, 
       :lead_time_id, 
-      :shipping_origin_prefecture_id,
-      :image
-      )
+      :shipping_origin_prefecture_id, 
+      :image).merge(user_id: current_user.id)
   end
 end
+
