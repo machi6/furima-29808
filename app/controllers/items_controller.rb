fb111966370698deb1eeef37e9b1534f557e2b00
machi6 <yosukehonma01@gmail.com>
2020-09-10 22:07:12 +0900

    非ログイン時は商品投稿画面に遷移しないよう変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :move_to_login, except: [:index]
+
   def index
-    #binding.pry
+    
   end
 
   def new
@@ -29,6 +31,11 @@ class ItemsController < ApplicationController
       :lead_time_id, 
       :shipping_origin_prefecture_id, 
       :image).merge(user_id: current_user.id)
+  end
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
 
