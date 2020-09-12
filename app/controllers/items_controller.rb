4b70941241750038c5f861341d36e9b1d0820caf
machi6 <yosukehonma01@gmail.com>
2020-09-12 21:30:14 +0900

    削除機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destoroy
-
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
