f3272dbaf0fdcd331af5af802612fef957cf53d1
machi6 <yosukehonma01@gmail.com>
2020-09-12 17:02:43 +0900

    rubocopを適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
