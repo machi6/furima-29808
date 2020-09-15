b152f362dbaced3f3d43d259dc72c50c69e0f6ac
machi6 <yosukehonma01@gmail.com>
2020-09-15 22:31:14 +0900

    orderコントローラーを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,20 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = OrderAddress.new(order_params)
 
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render "index"
+    end
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:user, :item, :order, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
   end
 end
