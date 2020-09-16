e3fd8c066448569361e4c97d2cf96ab3b68d0912
machi6 <yosukehonma01@gmail.com>
2020-09-16 23:20:16 +0900

    単体テストコードのためのFactoryBot記述を修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :order_address do
     user {1}
     item {1}
-    order {1}
     postal_code {'111-1111'}
     prefecture_id {2}
     city {'千代田区'}
     house_number {'千代田1-1'}
     building_name {'宮内庁'}
     phone_number {'11111111111'}
+    token {'1'}
   end
 end
