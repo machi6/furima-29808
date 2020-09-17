8126d6db270c7246b49002d6db0123d1e0d21bcc
machi6 <yosukehonma01@gmail.com>
2020-09-17 11:41:57 +0900

    バリデーションのリファクタリング（rubocop適用含む）

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,14 +2,19 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
-  validates :token,         presence: true
-  validates :user,          presence: true
-  validates :item,          presence: true
-  validates :postal_code,   presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city,          presence: true
-  validates :house_number,  presence: true
-  validates :phone_number,  presence: true, format: {with: /\A[0-9]{,11}\z/, message: 'is invalid. Input 11 or less numbers.'}
+  POSTAL_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/.freeze
+  PHONE_NUMBER_REGEX = /\A[0-9]{,11}\z/.freeze
+
+  with_options presence: true do
+    validates :token
+    validates :user
+    validates :item
+    validates :postal_code,   format: { with: POSTAL_CODE_REGEX, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: 'is invalid. Input 11 or less numbers.'}
+  end
 
   def save
     order = Order.create(user_id: user, item_id: item)
